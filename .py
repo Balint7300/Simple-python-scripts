e8642b431f93426cf8309122cf247fc4e1d8ecde
Balint7300 <tbalint685@gmail.com>
2024-11-10 12:17:29 +0100

    lesser greater equal
    A simple lesser greater equal in python 3.13

---
+++ .py
@@ -0,0 +1,14 @@
+a = input("Number 1: ")
+b = input("Number 2: ")
+
+if a > b:
+    print("A is bigger than B!")
+    input ("Press enter to leave!")
+
+if b > a:
+    print("B bigger than A!")
+    input ("Press enter to leave!")
+
+if a == b:
+    print("A is Equal with B")
+    input ("Press enter to leave!")
